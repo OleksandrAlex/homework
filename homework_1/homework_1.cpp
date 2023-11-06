a6cb6e9982d4ec14ad102ac20f17bbf2635fe93f
Owner <Owner@DESKTOP-LJD21HP>
2023-11-06 15:33:31 +0200

    Add project files.

---
+++ homework_1/homework_1.cpp
@@ -0,0 +1,235 @@
+
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Ukrainian");
+	/*for (int i = 1; i <= 10 ; i++)  
+	{
+		cout << i << endl;
+	}*/
+	
+
+	/*for (int i = 2; i <= 20; i++)
+	{
+		if (i % 2 == 0) {
+			cout << i;
+		}
+		
+	}*/
+
+
+	/*for (int i = 1; i <= 19; i++)
+		{
+			if (i % 2 != 0) {
+				cout << i << endl;
+			}
+		}*/
+
+
+	/*int a = 0;
+	for (int i = 1; i <= 100; i++)
+	{
+		a += i;
+		
+	}
+	cout << a;*/
+	
+
+	/*for (int i = 10; i > 0; i--)
+	{
+		cout << i << endl;
+	}*/
+
+
+	/*for (int i = 1; i <= 100; i++)
+	{
+		if (i % 7 == 0) {
+			cout << i << endl;
+		}
+	}*/
+
+
+	/*for (int i = 1; i <= 10; i++)
+	{
+		int a = 7;
+		int b = 1;
+		b = a * i;
+		cout << i <<" * " << a << " = " << b << endl;
+	}*/
+
+
+	/*float a = 1;
+	int b;
+	cout << " enter the positive number " << endl;
+	cin >> b;
+	for (int i = 1; i <= b; i++)
+	{
+		a = a * i;
+
+	}
+	cout << "the factorial of your number = " << a; */
+
+	/*int f = 0;
+	int f1 = 1;
+	int f2 = 1;
+	int a = 0;
+	int x;
+	for (int i = 1; f <= 1000; i++)
+	{
+		
+		f = f1 + f2;
+		cout << f << " ";
+		x = f1;
+		f1 = f2; 
+		f2 = x + f2;
+		a += f;
+		
+	}
+	cout << a << endl;*/
+
+	                   /*for (int i = 1; i <= 50; i++)
+	{
+		if (i %2 != 0 )
+	}
+
+	int a, b;
+	cout << "enter two numbers ";
+	cin >> a >> b;
+	for (int i = 0; i < length; i++)
+	{
+
+	                                     }*/ 
+
+
+	/*for (int i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0) {
+			cout << i << endl;
+		}
+	}*/
+
+   /* int a;
+    cout << "enter the number" << endl;
+    cin >> a;
+	int b = 0;
+	
+	for (int i = 1; a != 0; i++)
+	{
+		a = a / 10;
+		b++;
+	}
+	cout << b;*/
+
+
+    /*int a;
+	cout << "enter the number" << endl;
+	cin >> a;
+	int b = 0;
+	while (a > 0)
+	{
+		b += a % 10;
+		a = a / 10;
+		 
+	}
+	cout << "the sum of the digitals of the number is " << b;*/
+
+
+   /* int a;
+	cout << "enter the number ";
+	cin >> a;
+	int f = 0;
+	int f1 = 1;
+	int f2 = 1;
+	int x;
+	for (int i = 1; f < a; i++)
+	{
+		f = f1 + f2;
+		cout << f << endl;
+		x = f1;
+		f1 = f2;
+		f2 = x + f2;	
+	}
+	while (f < a)
+	{
+			f = f1 + f2;
+			cout << f << endl;
+			x = f1;
+			f1 = f2;
+			f2 = x + f2;
+	
+	}*/
+
+    /*int a, b, c;
+	cout << "enter two numbers " << endl;
+	cin >> a >> b;
+	while (a || b != 0)
+	{
+		
+		c = (a + b) / 2;
+		cout << "the average number is " << c << endl;
+		cout << "enter two numbers " << endl;
+		cin >> a >> b;
+	}*/
+ 
+
+   /* float a = 0;
+	for (int i = 1; i <= 100; i++)
+	{
+		a += i * i;
+	}
+	cout << "сума квадратів чисел від 1 до 100 =  " << a;*/
+
+
+   
+    /*for (int i = 1; i <= 20; i++)
+    {
+	    
+		cout << "квадрат числа - " << i << " = " << i * i << endl;
+    }*/
+
+//int a = 0;
+//for (int i = 1; i <= 5; i++)
+//{
+//	
+//	for (int j = 0; j <= a; j++)
+//	{
+//		cout << i;
+//		
+//	}
+//	
+//	cout << endl;
+//	a++;
+//}
+
+
+   /*int n, b = 0;
+   cout << "введіть число:";
+   cin >> n;
+   int z = 1;
+   for (int i = 1; i <= n; i++)
+   {
+	   b = b + i * z;
+	   z = -z;
+   }
+   cout << "сума = " << b;*/
+
+
+    
+ 
+    
+	
+
+    
+
+
+	
+
+	
+	 
+
+
+}
+
+
